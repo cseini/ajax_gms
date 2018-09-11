5c8a2b80789a264304b91950157da957783d431c
cseini <cseini@nate.com>
2018-09-11 09:51:55 +0900

    lambda rebuild

--- src/main/java/com/gms/web/controller/HomeController.java
+++ src/main/java/com/gms/web/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
+ 
 @Controller
 @SessionAttributes("context")
 public class HomeController {
